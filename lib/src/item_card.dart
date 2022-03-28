77b1e58303de144ac5d1b24bb88c70b4611edf4f
Incurafy <incurafy@gmail.com>
2022-03-28 10:26:21 +1000

    Added JSON and uuid package. Pushing to test fake API.

--- lib/src/item_card.dart
+++ lib/src/item_card.dart
@@ -1,9 +1,13 @@
 // item_card.dart
 
+import 'package:uuid/uuid.dart';
+
 // Defines an item card and its properties (many not listed)
 class ItemCard {
+  final Uuid id;
+  final int index;
   final String itemName;
   final String itemDesc;
 
-  const ItemCard(this.itemName, this.itemDesc);
+  const ItemCard(this.id, this.index, this.itemName, this.itemDesc);
 }
